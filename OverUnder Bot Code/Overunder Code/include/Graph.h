b9044579594cf18cf5c8f192cf1d3f39e024c3d7
Andrew Hilton <merpleton@gmail.com>
2023-07-27 13:37:20 -0600

    Started on Graphing

---
+++ OverUnder Bot Code/Overunder Code/include/Graph.h
@@ -0,0 +1,39 @@
+
+#pragma once
+
+void GraphDrive() 
+{
+
+
+
+}
+
+void GraphIntake() 
+{
+
+
+
+}
+
+void GraphDrivePos()
+{
+
+std::cout<<"";
+
+}
+
+void AnalyzeA() 
+{
+
+    GraphDrive();
+    GraphDrivePos();
+    std::cout<<std::endl;
+
+}
+
+void AnalyzeB() 
+{
+
+    GraphIntake();
+
+}
